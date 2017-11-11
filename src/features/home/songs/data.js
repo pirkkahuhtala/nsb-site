661877b3527973fc2a0ca4744d0814c3931e8f59
Pirkka Huhtala <pirkka.huhtala@weave.fi>
2017-11-11 08:55:20 +0200

    Add songs data

---
+++ src/features/home/songs/data.js
@@ -0,0 +1,53 @@
+import formAndVoid from './Novembersoundsbetter_-_Solace_in_Solitude_-_01_-_Form_and_Void.mp3'
+import living from './Novembersoundsbetter_-_Solace_in_Solitude_-_02_-_Living_on_the_Edge_of_Forever.mp3'
+import anEnemyOfFate from './Novembersoundsbetter_-_Solace_in_Solitude_-_03_-_An_Enemy_of_Fate.mp3'
+import thingsLeftBehind from './Novembersoundsbetter_-_Solace_in_Solitude_-_04_-_Things_Left_Behind.mp3'
+import weNeverStoodAChange from './novembersoundsbetter_-_Solace_in_Solitude_-_05_-_We_Never_Stood_a_Chance.mp3'
+import whatLiesBeneath from './Novembersoundsbetter_-_Solace_in_Solitude_-_06_-_What Lies Beneath.mp3'
+import aDayWillComeWhenYouWontBe from './Novembersoundsbetter_-_Solace_in_Solitude_-_07_-_A_Day_Will_Come_When_You_Won_t Be.mp3'
+import aLastOfHerKind from './Novembersoundsbetter_-_Solace_in_Solitude_-_08_-_The_Last_of_Her_Kind.mp3'
+
+const songs = [
+  {
+    id: 1,
+    file: formAndVoid,
+    name: 'Form and Void',
+  },
+  {
+    id: 2,
+    file: living,
+    name: 'Living on the Edge of Forever',
+  },
+  {
+    id: 3,
+    file: anEnemyOfFate,
+    name: 'An Enemy of Fate',
+  },
+  {
+    id: 4,
+    file: thingsLeftBehind,
+    name: 'Things Left Behind',
+  },
+  {
+    id: 5,
+    file: weNeverStoodAChange,
+    name: 'We Never Stood a Chance',
+  },
+  {
+    id: 6,
+    file: whatLiesBeneath,
+    name: 'What Lies Beneath',
+  },
+  {
+    id: 7,
+    file: aDayWillComeWhenYouWontBe,
+    name: 'A Day Will Come When You Won\'t Be',
+  },
+  {
+    id: 8,
+    file: aLastOfHerKind,
+    name: 'The Last of Her Kind',
+  },
+]
+
+export default songs
